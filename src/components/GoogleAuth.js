9300827c3e72b5dab66ff79df9304348be923245
ayezinzu <59067019+ayezinzu@users.noreply.github.com>
2021-10-18 19:39:49 +0530

    changes

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -21,7 +21,7 @@ class GoogleAuth extends React.Component {
 
   onAuthChange = (isSignedIn) => {
     if (isSignedIn) {
-      this.props.signIn();
+      this.props.signIn(this.auth.currentUser.get().getId());
     } else {
       this.props.signOut();
     }
@@ -67,8 +67,9 @@ class GoogleAuth extends React.Component {
 }
 
 const mapStateToProps = (state) => {
+  console.log(state);
   return {
-    isSignedIn: state.isSignedIn,
+    isSignedIn: state.auth.isSignedIn,
   };
 };
 
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,13 +1,14 @@
 const INITIAL_STATE = {
   isSignedIn: null,
+  userId: null,
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case "SIGN_IN":
+      return { ...state, isSignedIn: true, userId: action.payload.userId };
     case "SIGN_OUT":
-      return { ...state, isSignedIn: true };
-    case "SIGN_OUT":
-      return { ...state, isSignedIn: false };
+      return { ...state, isSignedIn: false, userId: null };
     default:
       return state;
   }
